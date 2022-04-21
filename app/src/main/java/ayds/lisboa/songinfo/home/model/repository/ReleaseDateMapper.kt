99a0b2af770d335f1083705f427c3fe0d4a5c8c9
VictoriaMartinSahagun <vams.00@hotmail.com>
2022-04-20 22:44:32 -0300

    creación constantes
    Creación de constantes para day, month e year.

--- app/src/main/java/ayds/lisboa/songinfo/home/model/repository/ReleaseDateMapper.kt
+++ app/src/main/java/ayds/lisboa/songinfo/home/model/repository/ReleaseDateMapper.kt
@@ -7,12 +7,15 @@ interface ReleaseDateMapper {
 }
 
 internal class ReleaseDateMapperImpl: ReleaseDateMapper {
+    val day = "day"
+    val month = "month"
+    val year = "year"
 
     override fun mapReleaseDatePrecision(datePrecision: String): ReleaseDatePrecision {
         return when(datePrecision) {
-            "day" -> ReleaseDatePrecision.DAY
-            "month" -> ReleaseDatePrecision.MONTH
-            "year" -> ReleaseDatePrecision.YEAR
+            day -> ReleaseDatePrecision.DAY
+            month -> ReleaseDatePrecision.MONTH
+            year -> ReleaseDatePrecision.YEAR
             else -> ReleaseDatePrecision.EMPTY
         }
     }
