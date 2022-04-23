3a38132a7c41e8d0e860bb60cf478fa3af6dd1e3
Yanina E. Rivera <yanely299@gmail.com>
2022-04-23 16:56:48 -0300

    Division en funciones mas chicas
    Sacamos la interfaz
    Dividimos en funciones mas chicas
    Definimos constantes
    
    Co-Authored-By: florlosta <71231076+florlosta@users.noreply.github.com>

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
@@ -15,23 +15,28 @@ import android.text.Html
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
-import com.google.gson.JsonElement
 import retrofit2.Response
 import java.io.IOException
 import java.lang.StringBuilder
+import android.widget.Button
 
-interface OtherInfoWindow {
-    fun onCreate(savedInstanceState: Bundle?)
-}
 
 const val ARTIST_NAME_EXTRA = "artistName"
 private const val AUDIO_SCROBBLER_URL = "https://ws.audioscrobbler.com/2.0/"
 private const val IMAGE_URL_LASTFM = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/320px-Lastfm_logo.svg.png"
+private const val ARTIST = "artist"
+private const val ARTIST_BIOGRAPHY = "bio"
+private const val ARTIST_BIOGRAPHY_EXTRACT = "content"
+private const val ARTIST_BIOGRAPHY_URL = "url"
 
-class OtherInfoWindowImpl : AppCompatActivity(), OtherInfoWindow {
+class OtherInfoWindow : AppCompatActivity() {
 
     private lateinit var biographyTextView: TextView
+    private lateinit var openUrlButton: Button
+
     private lateinit var dataBase: DataBase
+
+    private lateinit var BiographyJsonObject: JsonObject
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,21 +48,33 @@ class OtherInfoWindowImpl : AppCompatActivity(), OtherInfoWindow {
 
     private fun initProperties() {
         biographyTextView = findViewById(R.id.biographyTextView)
+        openUrlButton = findViewById<View>(R.id.openUrlButton) as Button
     }
 
     private fun getArtistInfo(artistName: String?) {
         Thread {
-            val finalText = createBiography(artistName)
-            runOnUiThread {
-                Picasso.get().load(IMAGE_URL_LASTFM).into(findViewById<View>(R.id.imageView) as ImageView)
-                biographyTextView!!.text = Html.fromHtml(finalText)
-            }
+            val artistBiography = createBiography(artistName)
+            updateArtistImage()
+            updateArtistBiography(artistBiography)
         }.start()
     }
 
+    private fun updateArtistImage() {
+        runOnUiThread {
+            Picasso.get().load(IMAGE_URL_LASTFM)
+                .into(findViewById<View>(R.id.imageView) as ImageView)
+        }
+    }
+
+    private fun updateArtistBiography(biographyText: String) {
+        runOnUiThread {
+            biographyTextView!!.text = Html.fromHtml(biographyText)
+        }
+    }
+
     private fun createBiography(artistName: String?): String {
-        var biographyText = dataBase.getInfo(dataBase, artistName)
-        biographyText = if (biographyText != null) { // exists in db
+        var biographyText = dataBase.getInfo(artistName)
+        biographyText = if (biographyText != null) {
             "[*]$biographyText"
         } else {
             getArtistBiographyFromLastFM(artistName)
@@ -68,40 +85,56 @@ class OtherInfoWindowImpl : AppCompatActivity(), OtherInfoWindow {
     private fun getLastFMAPI() = initRetrofit().create(LastFMAPI::class.java)
 
     private fun getArtistBiographyFromLastFM(artistName: String?): String {
-        var biographyText: String = ""
+            getArtistJSon(artistName)
+            setListenerUrlButton(getBiographyUrl())
+            return getArtistBiographyText(artistName)
+    }
+
+    private fun getArtistBiographyText(artistName: String?):String {
+        var biographyText: String
+        if (getBiographyExtract() == "") {
+            biographyText = "No Results"
+        } else {
+            biographyText = getBiographyExtract().replace("\\n", "\n")
+            biographyText = textToHtml(biographyText, artistName)
+            dataBase.saveArtist(artistName, biographyText)
+        }
+        return biographyText
+    }
+
+    private fun getArtistJSon(artistName: String?){
         val callResponse: Response<String>
         try {
             callResponse = getLastFMAPI().getArtistInfo(artistName).execute()
             val gson = Gson()
-            val jobj = gson.fromJson(callResponse.body(), JsonObject::class.java)
-            val artist = jobj["artist"].asJsonObject
-            val bio = artist["bio"].asJsonObject
-            val extract = bio["content"]
-            val url = artist["url"]
-
-            if (extract == null) {
-                biographyText = "No Results"
-            } else {
-                biographyText = extract.asString.replace("\\n", "\n")
-                biographyText = textToHtml(biographyText, artistName)
-
-                // save to DB  <o/
-                dataBase.saveArtist(dataBase, artistName, biographyText)
-            }
-            val urlString = jsonElementToString(url)
-
-            findViewById<View>(R.id.openUrlButton).setOnClickListener {
-                val intent = Intent(Intent.ACTION_VIEW)
-                intent.data = Uri.parse(urlString)
-                startActivity(intent)
-            }
-           return  biographyText
-        } catch (e1: IOException) {
+            BiographyJsonObject = gson.fromJson(callResponse.body(), JsonObject::class.java)
+        }
+         catch (e1: IOException) {
             Log.e("TAG", "Error $e1")
             e1.printStackTrace()
-            return ""
         }
-        //return biographyText
+    }
+
+    private fun getBiographyExtract(): String {
+        val artist = BiographyJsonObject [ARTIST].asJsonObject
+        val bio = artist[ARTIST_BIOGRAPHY].asJsonObject
+        return bio[ARTIST_BIOGRAPHY_EXTRACT].asString
+    }
+
+    private fun getBiographyUrl(): String {
+        val artist = BiographyJsonObject [ARTIST].asJsonObject
+        return artist[ARTIST_BIOGRAPHY_URL].asString
+    }
+    private fun setListenerUrlButton(urlBiography: String){
+        openUrlButton.setOnClickListener {
+            navigateToUrl(urlBiography)
+        }
+    }
+
+    private fun navigateToUrl(urlBiography: String){
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(urlBiography)
+        startActivity(intent)
     }
 
     private fun initRetrofit() = Retrofit.Builder()
@@ -111,24 +144,23 @@ class OtherInfoWindowImpl : AppCompatActivity(), OtherInfoWindow {
 
     private fun open(artist: String?) {
         dataBase = DataBase(this)
-        dataBase.saveArtist(dataBase, "test", "sarasa")
+        dataBase.saveArtist("test", "sarasa")
         getArtistInfo(artist)
-    }
-
-    private fun jsonElementToString(jsonElement: JsonElement): String {
-        return jsonElement.asString
     }
 
     private fun textToHtml(text: String, term: String?): String {
         val builder = StringBuilder()
         builder.append("<html><div width=400>")
         builder.append("<font face=\"arial\">")
-        val textWithBold = text
-            .replace("'", " ")
-            .replace("\n", "<br>")
-            .replace("(?i)" + term!!.toRegex(), "<b>" + term.uppercase() + "</b>")
-        builder.append(textWithBold)
+        builder.append(artistBiographyTextWithBold(text, term))
         builder.append("</font></div></html>")
         return builder.toString()
+    }
+
+    private fun artistBiographyTextWithBold(text: String, term:String?): String{
+        return text
+                .replace("'", " ")
+                .replace("\n", "<br>")
+                .replace("(?i)" + term!!.toRegex(), "<b>" + term.uppercase() + "</b>")
     }
 }
