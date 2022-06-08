92be1707effa48bce957f8f89abc62d956758336
Emmanuel <elagarriguelazarte@doximity.com>
2022-06-08 09:24:37 -0300

    Remove libs local copy

--- libs/AyDS-NewYork2-Submodule/src/main/java/ayds/newyork2/newyorkdata/nytimes/NYTimesToArtistResolver.kt
+++
@@ -1,6 +0,0 @@
-package ayds.newyork2.newyorkdata.nytimes
-
-interface NYTimesToArtistResolver {
-
-    fun getArtistFromExternalData(serviceData: String?, artistName: String) : NYTimesArtistInfo?
-}
--- libs/wikipedia-module/src/main/java/ayds/winchester2/wikipedia/WikipediaArticle.kt
+++
@@ -1,7 +0,0 @@
-package ayds.winchester2.wikipedia
-
-data class WikipediaArticle(
-     val source: String,
-     val description: String,
-     val sourceLogo: String
-)
